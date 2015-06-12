2a99f8b3337253861ea09c88c3330b612f73cd5b
jack8409201 <jack8409201@gmail.com>
2015-06-12 21:00:44 +0800

    Create Rule class
    建一個說明遊戲規則的frame,按下確定後即開始遊戲

---
+++ Rule.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rule extends JFrame{
+	private JButton jbtSure = new JButton("確定");
+	
+	public Rule(){
+		setLayout(null);
+		
+		JTextArea jtaRule = new JTextArea(" 記憶配對遊戲(對對碰) : \n\n"
+								  +	"               共有6對12張神奇寶貝球牌, \n"
+								  + "               請在最短時間及最少次數內, \n"
+								  + "               依照相同的神奇寶貝做配對 !!");
+		jtaRule.setFont(new Font("TimesRoman",Font.BOLD,15));
+		jtaRule.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+		jtaRule.setBounds(70, 30, 350, 130);
+		add(jtaRule);
+		
+		jbtSure.setBounds(190, 190, 100, 40);
+		add(jbtSure);
+		
+		jbtSure.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				Game game = new Game();
+				game.setVisible(true);
+				dispose();
+			}
+		});
+		
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new Rule();
+		frame.setTitle("U10316046_1032JavaProject");
+		frame.setLayout(null);
+		frame.setBounds(100,100,500,300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+
+	}
+
+}
